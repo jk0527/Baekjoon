b990350581787bfe599e7dacc6a37d1e7f73d03d
jk0527 <78495599+jk0527@users.noreply.github.com>
2022-02-25 22:47:24 +0900

    Add files via upload

---
+++ 11279.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	int n, x;
+	priority_queue<int> pq;
+
+	cin >> n;
+	while (n--) {
+		cin >> x;
+		if (x == 0) {
+			if (pq.empty())
+				cout << 0 << '\n';
+			else {
+				cout << pq.top() << '\n';
+				pq.pop();
+			}
+		}
+		else
+			pq.push(x);
+	}
+}
