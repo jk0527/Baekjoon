a8eec7e5a2098f81bc3cba232aed1b2210730330
jk0527 <78495599+jk0527@users.noreply.github.com>
2022-07-17 22:35:08 +0900

    Add files via upload

---
+++ 2579.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int maxScore(int n, vector<int>& score) {
+	vector<int> dp(n + 1, 0);
+	dp[1] = score[1];
+	dp[2] = score[1] + score[2];
+	for (int i = 3; i <= n; i++)
+		dp[i] = max(dp[i - 2], dp[i - 3] + score[i - 1]) + score[i];
+	return dp[n];
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	vector<int> score(n + 1, 0);
+	for (int i = 1; i <= n; i++)
+		cin >> score[i];
+	cout << maxScore(n, score) << '\n';
+	return 0;
+}
